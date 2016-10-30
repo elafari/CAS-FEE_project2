ec41e227b7d9db6a46f665e4028ce4ff3a336035
Rafael Bamert <rafael.bamert@gmail.com>
2016-10-30 23:50:45 +0100

    refactored home.component (html/scss)

--- src/app/pages/home.component.ts
+++ src/app/pages/home.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  templateUrl: './home.component.html'
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 
-export class HomeComponent {}
+export class HomeComponent {
+}
