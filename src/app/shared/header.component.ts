389f64817cebc4e5b57d91107d7c7abac695865f
Rafael Bamert <rafael.bamert@gmail.com>
2016-11-05 16:03:11 +0100

    refactored header markup and styling - part2

--- src/app/shared/header.component.ts
+++ src/app/shared/header.component.ts
@@ -9,6 +9,8 @@ import { AuthService } from "../auth/auth.service";
 import { ConfigService } from "./config.service";
 import { LoggerService } from "../log/logger.service";
 
+declare var jQuery: any;
+
 @Component({
     selector: 'disease-diary-header',
     templateUrl: 'header.component.html',
@@ -26,29 +28,33 @@ export class HeaderComponent implements OnInit {
     };
 
     ngOnInit() {
-      try {
-        this.af.auth.subscribe(auth => {
-          if (auth) {
-            this.af.database.object(ConfigService.firebaseDbConfig.db + ConfigService.firebaseDbConfig.users + '/' + auth.uid).subscribe((user) => {
-              this.loggedInUserName = user.name;
-              this.loggedInUserAdmin = user.admin;
-              this.logger.info("[header] - ngOnInit - user : " + user.name + ' admin: ' + user.admin);
+        try {
+            this.af.auth.subscribe(auth => {
+                if (auth) {
+                    this.af.database.object(ConfigService.firebaseDbConfig.db + ConfigService.firebaseDbConfig.users + '/' + auth.uid).subscribe((user) => {
+                        this.loggedInUserName = user.name;
+                        this.loggedInUserAdmin = user.admin;
+                        this.logger.info("[header] - ngOnInit - user : " + user.name + ' admin: ' + user.admin);
+                    });
+                }
             });
-          }
-        });
-      } catch(e) {
-        this.logger.error("[header] - ngOnInit - error: " + e);
-      }
+        } catch (e) {
+            this.logger.error("[header] - ngOnInit - error: " + e);
+        }
     };
 
-    onLogout() {
-      try {
-        this.authService.logout();
-        this.logger.info("[header] - onLogout: logged out");
-        
-        this.router.navigate(['']);
-      } catch(e) {
-        this.logger.error("[header] - onLogout - error: " + e);
-      }
+    onLogout(nav) {
+        try {
+            this.authService.logout();
+            this.collapseNav(nav);
+            this.logger.info("[header] - onLogout: logged out");
+            this.router.navigate(['']);
+        } catch (e) {
+            this.logger.error("[header] - onLogout - error: " + e);
+        }
+    };
+
+    collapseNav(nav) {
+        jQuery(nav).collapse('hide');
     };
 }
