84a2c6984f7bb2578cdfdff5de228d5b2d386b1c
Rafael Bamert <rafael.bamert@gmail.com>
2016-12-09 09:45:21 +0100

    added loading spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector   : 'app-disease',
-    templateUrl: 'app.component.html'
+    templateUrl: 'app.component.html',
+    styleUrls  : ['app.scss']
 })
 export class AppComponent {
 }
